4618279a7059801a1a1ae9cfc24c5573591bfd35
JacobBarnard <jacob.barnard21@gmail.com>
2020-12-01 15:50:23 -0700

    login page, vuex

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routes.js'
 import axios from 'axios'
+import store from './store.js'
 
 axios.defaults.baseURL = "https://cis410-fa20-rockwellapi.azurewebsites.net"
 
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
